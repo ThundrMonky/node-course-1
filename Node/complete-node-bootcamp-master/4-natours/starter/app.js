a0ccef3cb04825a500cbdd1fab6c5f3dc327eab2
ThundrMonky <pjack1@vt.edu>
2023-03-14 19:31:06 -0400

    Handling patch request

--- Node/complete-node-bootcamp-master/4-natours/starter/app.js
+++ Node/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -44,6 +44,22 @@ app.post('/api/v1/tours', (req, res) => {
   tours.push(newTour);
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Did not work',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated Tour Here>',
+    },
+  });
+});
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
