f19598db55c7900059cd20c88a7cadce0e78d147
ThundrMonky <pjack1@vt.edu>
2023-03-20 15:56:41 -0400

    Environment variables, ESLint and Prettier

--- Node/complete-node-bootcamp-master/4-natours/starter/app.js
+++ Node/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -9,9 +9,13 @@ const app = express();
 
 // MIDDLEWARES (APPLY TO ALL ROUTES)
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the Middleware 👋');
