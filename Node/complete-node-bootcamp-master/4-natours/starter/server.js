3ce4449edc5f2d6523a7a60783e0921f862acef8
ThundrMonky <pjack1@vt.edu>
2023-03-20 16:04:46 -0400

    Minor cleanup, Express lesson complete

--- Node/complete-node-bootcamp-master/4-natours/starter/server.js
+++ Node/complete-node-bootcamp-master/4-natours/starter/server.js
@@ -1,8 +1,10 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 // console.log(process.env);
 
 const app = require('./app');
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
