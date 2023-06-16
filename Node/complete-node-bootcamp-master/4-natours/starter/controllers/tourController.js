08c66fca466ee173a09230d6cddced5ca7f12559
ThundrMonky <pjack1@vt.edu>
2023-06-16 15:11:15 -0400

    Intro to Mongo DB complete and intro to mongoose

--- Node/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
+++ Node/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
@@ -1,19 +1,20 @@
-const fs = require('fs');
+// const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
 
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour ID is: ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Did not work',
-    });
-  }
-  next();
-};
+// exports.checkID = (req, res, next, val) => {
+//   console.log(`Tour ID is: ${val}`);
+//   if (req.params.id * 1 > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Did not work',
+//     });
+//   }
+//   next();
+// };
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -32,34 +33,34 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
+  // const newId = tours[tours.length - 1].id + 1;
+  // const newTour = Object.assign({ id: newId }, req.body);
+  // tours.push(newTour);
+  // res.status(201).json({
+  //   status: 'success',
+  //   data: {
+  //     tour: newTour,
+  //   },
+  // });
 };
 
 exports.updateTour = (req, res) => {
