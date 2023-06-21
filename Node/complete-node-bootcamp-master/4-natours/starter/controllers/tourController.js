abbe782eef9c2eda5f6c9d37ca61b31762dd7b68
ThundrMonky <pjack1@vt.edu>
2023-06-20 21:39:10 -0400

    96-98 Advanced filtering, sorting and limiting

--- Node/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
+++ Node/complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
@@ -1,43 +1,40 @@
-// const fs = require('fs');
 const { query } = require('express');
 const Tour = require('./../models/tourModel');
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-// exports.checkID = (req, res, next, val) => {
-//   console.log(`Tour ID is: ${val}`);
-//   if (req.params.id * 1 > tours.length) {
-//     return res.status(404).json({
-//       status: 'fail',
-//       message: 'Did not work',
-//     });
-//   }
-//   next();
-// };
-
-// VALIDATION THAT NAME AND PRICE WERE IN THE REQUEST BODY
-// MONGOOSE TAKES CARE OF THIS VALIDATION NOW
-// exports.checkBody = (req, res, next) => {
-//   if (!req.body.name || !req.body.price) {
-//     console.log('Status check worked 👍');
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'Missing name or price in body',
-//     });
-//   }
-//   next();
-// };
-
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
+
     // BUILD QUERY
+    // 1a) FILTERING
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // 1b) ADVANCED FILTERING
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+
+    let query = Tour.find(JSON.parse(queryString));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
@@ -47,13 +44,6 @@ exports.getAllTours = async (req, res) => {
     //   duration: 5,
     //   difficulty: 'easy',
     // });
-
-    // FILTER QUERY USING MONGOOSE
-    // const query = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
 
     // SEND RESPONSE
     res.status(200).json({
