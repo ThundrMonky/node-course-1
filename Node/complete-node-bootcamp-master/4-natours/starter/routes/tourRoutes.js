76b142bd778ce3f71300f91ce5856e0af06d50fb
ThundrMonky <pjack1@vt.edu>
2023-06-22 10:29:29 -0400

    100-101 Aliasing and Refactor to class

--- Node/complete-node-bootcamp-master/4-natours/starter/routes/tourRoutes.js
+++ Node/complete-node-bootcamp-master/4-natours/starter/routes/tourRoutes.js
@@ -4,12 +4,9 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-// router.param('id', tourController.checkID);
-
-// Create a checkBody middleware
-// Check if body contains the name and price property
-// If not, send back 400 error (bad request)
-// Add it to the post handler stack
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
   .route('/')
