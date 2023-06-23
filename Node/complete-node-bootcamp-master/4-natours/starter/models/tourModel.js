d23758e61337cc240ddf6d7aaceb7cf403de085f
ThundrMonky <pjack1@vt.edu>
2023-06-23 11:49:47 -0400

    106 Query Middleware

--- Node/complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
+++ Node/complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
@@ -55,6 +55,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -82,6 +86,18 @@ tourSchema.pre('save', function (next) {
 //   next();
 // });
 
+// Query Middleware
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(docs);
+  console.log(`Query took ${Date.now() - this.start} milliseconds to complete`);
+  next();
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
