2abb884304de29cb55b62e5d3a922418e45ff045
ThundrMonky <pjack1@vt.edu>
2023-07-17 13:27:48 -0400

    Updating?

--- Node/complete-node-bootcamp-master/4-natours/starter/app.js
+++ Node/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -17,10 +17,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -29,6 +25,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(400).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 // START SERVER
 
